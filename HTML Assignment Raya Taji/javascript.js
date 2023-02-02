9fe58e3752b2409cadb764dfcee682d34d84c9be
taji-raya <124244239+taji-raya@users.noreply.github.com>
2023-02-02 11:08:17 +0300

    Add files via upload

---
+++ HTML Assignment Raya Taji/javascript.js
@@ -0,0 +1,13 @@
+const prev = document.querySelector(".prevButton");
+const next = document.querySelector(".nextButton");
+const track = document.querySelector(".swiperContent");
+const containerWidth = document.querySelector(".swiperContainer").offsetWidth;
+
+next.addEventListener('click', () => {
+
+    track.style.transform = `translateX(-${containerWidth}px)`;
+})
+prev.addEventListener('click', () => {
+
+    track.style.transform = `translateX(-${0}px)`;
+})
