2985f7752ffef0efbbae5c3c8374f3cc251b2a42
taji-raya <124244239+taji-raya@users.noreply.github.com>
2023-02-07 13:36:17 +0300

    Fixing put and post

--- HTML Assignment Raya Taji/dataScript.js
+++ HTML Assignment Raya Taji/dataScript.js
@@ -13,6 +13,7 @@ fetch('https://jsonplaceholder.typicode.com/posts')
     .then((completeData) => {
         //to display api data into my cards:
         let data1 = "";
+
         //using map method, ill access the data from the api 
         //it takes 3 arguments: 1 is the current value, 2 is index number and 3 is the array we're working on
         //instead of having a card container in the html file, we create it in the js so it creates a new card everytime
@@ -24,17 +25,16 @@ fetch('https://jsonplaceholder.typicode.com/posts')
                         <h1 class="dataTitle">Title:  ${values.title}</h1>
                         <br>
                         <p class="dataBody">${values.body}</p>
-                       
                     </div>
                 </div>`
 
         });
         document.getElementById("dataContainer").innerHTML = data1;
     });
+
     document.getElementById("addPost").addEventListener('submit', addPost);
-    document.getElementById("editPost").addEventListener('submit', editPost);
 
-
+//To add a post
 function addPost(e) {
     e.preventDefault(); //prevents it from submetting to an actual file
 
@@ -56,27 +56,25 @@ function addPost(e) {
         
     })
         .then((response) => response.json())
-        .then((json) => {
-            let data2 = " ";
-            json.forEach(function(post){
-                data2 = `
-                    <div class='card' >
-                        <div class='cardContent' >
-                            <h1 class="dataTitle">Title:  ${values.title}</h1>
-                            <br>
-                            <p class="dataBody">${values.body}</p>
-                           
-                        </div>
-                    </div>`
-    
-                
-            });
-            document.getElementById("dataContainer").innerHTML = data2;
-            });
+        .then((completeData) => {
+            let data1 = "";
+        completeData.map((values) => {
+            data1 += `
+                <div class='card' >
+                    <div class='cardContent' >
+                        <h1 class="dataTitle">Title:  ${values.title}</h1>
+                        <br>
+                        <p class="dataBody">${values.body}</p>
+                    </div>
+                </div>`
+        });
+        document.getElementById("dataContainer").innerHTML = data1;
+    });
             
     
 }
-
+//document.getElementById("editPost").addEventListener('submit', editPost);
+//To edit the post
 function editPost(e){
     e.preventDefault();
     fetch('https://jsonplaceholder.typicode.com/posts/1', {
@@ -92,22 +90,20 @@ function editPost(e){
   },
 })
   .then((response) => response.json())
-  .then((json) => {
-    let data3 = " ";
-    json.forEach(function(post){
-        data3 = `
-            <div class='card' onclick="cardInfo()">
-                <div class='cardContent' >
-                    <h1 class="dataTitle">Title:  ${values.title}</h1>
-                    <br>
-                    <p class="dataBody">${values.body}</p>
-                   
-                </div>
-            </div>`
+  .then((completeData) => {
+    let data1 = " ";
+        completeData.map((values) => {
+            data1 += `
+                <div class='card' >
+                    <div class='cardContent' >
+                        <h1 class="dataTitle">Title:  ${values.title}</h1>
+                        <br>
+                        <p class="dataBody">${values.body}</p>
+                    </div>
+                </div>`
 
-        
-    });
-    document.getElementById("dataContainer").innerHTML = data3;
+        });
+        document.getElementById("dataContainer").innerHTML = data1;
     });
 
 }
